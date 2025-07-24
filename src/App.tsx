9b4e1992d87775ed8adb9506b3bb5afa3e94e0a7
benjag22 <benjagonzalez2022@udec.cl>
2025-07-24 17:42:11 -0400

    feat: add href's and missing components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import AboutSection from "./sections/AboutSection.tsx";
+import TechSection from "./sections/TechSections.tsx"
+import WorkSection from "./sections/WorkSection.tsx";
 import NavBar from "./components/NavBar.tsx";
 
 import './App.css'
 
 function App() {
-
+    const sectionsIds = {
+        aboutSection: "about",
+        techSection: "tech",
+        workSection: "work"
+    }
     return (
         <>
-            <NavBar/>
-            <AboutSection/>
+            <NavBar
+                aboutHref={"#"+sectionsIds.aboutSection}
+                techHref={"#"+sectionsIds.techSection}
+                workHref={"#"+sectionsIds.workSection}
+            />
+            <AboutSection id={sectionsIds.aboutSection}/>
+            <TechSection id={sectionsIds.techSection}/>
+            <WorkSection id={sectionsIds.workSection}/>
         </>
     )
 }
