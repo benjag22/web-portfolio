40700e1cb797b615ba1d2f3f1c3fb744590dbf63
benjag22 <benjagonzalez2022@udec.cl>
2025-07-24 17:24:09 -0400

    feat: add href props to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,32 @@
 import cn from "../utils/cn"
+type NavBarProps = {
+    aboutHref: string;
+    techHref: string;
+    workHref: string;
+}
 
-export default function NavBar() {
+export default function NavBar(props: NavBarProps) {
     return (
         <nav className={cn(
             "sticky top-0 z-10",
-            "bg-gradient-to-b from-sky-950 to-[#70b0a8]",
+            "bg-gradient-to-b from-sky-950 to-[#70b0a8] opacity-95",
             "h-16"
         )}>
             <ul className={cn("flex flex-row justify-between items-center h-full text-xl px-4")}>
                 <li>
-                    <p className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
+                    <a href={props.aboutHref} className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
                         About me
-                    </p>
+                    </a>
                 </li>
                 <li>
-                    <p className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
+                    <a href={props.techHref} className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
                         Tech Stack
-                    </p>
+                    </a>
                 </li>
                 <li>
-                    <p className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
+                    <a href={props.workHref} className={cn("py-3 px-6 cursor-pointer hover:opacity-80")}>
                         Work
-                    </p>
+                    </a>
                 </li>
             </ul>
         </nav>
