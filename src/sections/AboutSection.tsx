d8b72cfb53317de766aa49ba3658c246e02846e8
benjag22 <benjagonzalez2022@udec.cl>
2025-07-24 23:44:36 -0400

    feat: add more styles to about section and animations.

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -1,5 +1,10 @@
 import cn from "../utils/cn"
 import {FaGithub, FaLinkedin} from "react-icons/fa"
+import { TbFileCv } from "react-icons/tb";
+import {metadata} from "../metadata.ts"
+
+import styles from "./AboutSection.module.css"
+
 type AboutSectionProps = {
     id: string
 }
@@ -13,25 +18,63 @@ export default function AboutSection(props: AboutSectionProps) {
         <section id= {props.id} className={cn(
             "font-roboto-condensed",
             "h-screen",
-            "flex flex-col"
         )}>
             <div className={cn(
-                "h-full w-full bg-sky-950 text-xl",
-                "flex flex-col justify-center items-center"
+                "h-full w-full bg-[#010621]",
+                "grid [grid-template-rows:30%_50%_20%] gap-2"
             )}>
-                <p className={cn(
-                    "font-roboto-condensed",
-                    "text-white text-center mb-4"
-                )}>
-                    Hello, {props.id}
-                </p>
+                <div className={cn("flex w-full items-start justify-start p-8")}>
+                    <h1 className={cn(
+                        "font-roboto-condensed",
+                        "italic text-5xl text-white p-6",
+                        "border-b-2 border-cyan-600 shadow-md shadow-cyan-800"
+                    )}>
+                        {"< Acerca de Benjamín González />"}
+                    </h1>
+                </div>
                 <div className={cn(
-                    "flex flex-row"
+                    "grid grid-cols-2 gap-2 px-8 pb-8"
+                )}>
+                    <div className={styles.borderContainer}>
+                        <div className={styles.animatedBorder}>
+                            <div className={styles.borderTrail}></div>
+                        </div>
+
+                        <div className={cn(
+                            styles.contentWithBorder,
+                            "text-xl"
+                        )}>
+                            <p className={cn(
+                                "font-roboto-condensed",
+                                "text-white text-center py-8"
+                            )}>
+                                {metadata.aboutMeParagraph1}
+                            </p>
+                            <p className={cn(
+                                "font-roboto-condensed",
+                                "text-white text-center py-8"
+                            )}>
+                                {metadata.aboutMeParagraph2}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={cn(
+                        "flex items-center justify-center"
+                    )}>
+                        Aca va la foto.
+                    </div>
+                </div>
+                <div className={cn(
+                    "flex flex-row justify-center"
                 )}>
                     <FaGithub onClick={() => goTo("https://github.com/benjag22")} className={cn(
                         "text-6xl text-white hover:text-gray-300 cursor-pointer transition-colors"
                     )}/>
                     <FaLinkedin onClick={() => goTo("https://www.linkedin.com/in/benjags/")} className={cn(
+                        "text-6xl text-white hover:text-gray-300 cursor-pointer transition-colors"
+                    )}/>
+                    <TbFileCv width={200} className={cn(
                         "text-6xl text-white hover:text-gray-300 cursor-pointer transition-colors"
                     )}/>
                 </div>
