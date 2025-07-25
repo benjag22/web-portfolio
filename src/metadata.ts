db4ae252408721cfa78aa9b7bbfb3736e3ca3d0f
benjag22 <benjagonzalez2022@udec.cl>
2025-07-24 23:40:16 -0400

    feat: add about info

---
+++ src/metadata.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+    aboutMeParagraph1: "Soy Benjamín González, estudiante de Ingeniería Civil Informática" +
+        " en la Universidad de Concepción de Chile. A lo largo mi formación y experiencia entre las diversas áreas de la informática," +
+        " la que ha marcado interes y motivación ha sido el desarrollo de software.",
+
+    aboutMeParagraph2: "Por eso, me especializo como Desarrollador Full Stack,..."
+}
